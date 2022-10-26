6aaea1513d85dcad219d2faf8ffa2d939f36a535
Allanymous <allansilvafagundes@gmail.com>
2022-10-26 17:55:26 -0200

    add file allan.html

---
+++ allan.js
@@ -0,0 +1,10 @@
+let oarcela1 = 15
+let parcela2 = 20
+let resultado = 0
+var parcela3 = 150
+var parcela4 = 200
+var resultado2 = 0
+let meunome = "Allan"
+var meunome2 = "Allan Fagundes Da Silva Santos"
+let parcelajunta = parcela1
+//exercicio calculadeira 1-usando prompt | 
